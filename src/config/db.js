75a674aaaecb0e266cadb3a0d4340f9baf4ecbab
Zalerya <153430442+Martzcode@users.noreply.github.com>
2026-02-28 12:35:24 +0300

    modification URL mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://centrecommercial:MotDePasseFort123@localhost:27017/centrecommercial?authSource=admin'
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     console.log(`Database: ${conn.connection.name}`);
   } catch (error) {
